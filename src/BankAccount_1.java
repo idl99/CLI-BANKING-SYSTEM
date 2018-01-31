d67c21b3995c30d8a890de175356f0ebb20c2175
idl99 <ihandilnath2012@gmail.com>
2018-01-31 21:50:13 +0530

    [BankAccount_1.java] added functionality to view account balance

--- src/BankAccount_1.java
+++ src/BankAccount_1.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class BankAccount_1 {
@@ -6,62 +8,86 @@ public class BankAccount_1 {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.print(
-                "========================================\n"+
-                "WELCOME TO INTERBANKING PTY SYSTEM\n"+
-                "========================================\n"+
-                "1. CREATE NEW BANK ACCOUNT \n"+
-                "2. VIEW ACCOUNT BALANCE \n"+
-                "\nPLEASE ENTER THE NUMBER CORRESPONDING TO YOUR DESIRED COMMAND: "
-        );
+        List<BankAccount> listOfBankAcc = new ArrayList<>();
 
-        int opt = sc.nextInt();
+        while (true) {
+            System.out.print(
+                    "========================================\n"+
+                    "WELCOME TO INTERBANKING PTY SYSTEM\n"+
+                    "========================================\n"+
+                    "1. CREATE NEW BANK ACCOUNT \n"+
+                    "2. VIEW ACCOUNT BALANCE \n"+
+                    "\nPLEASE ENTER THE NUMBER CORRESPONDING TO YOUR DESIRED COMMAND OR 0 TO EXIT: "
+            );
 
-        System.out.println();
+            int opt = sc.nextInt();
 
-        switch (opt){
+            if (opt == 0) break;
 
-            case 1:
-                System.out.println(
-                        "==============================\n" +
-                        "NEW BANK ACCOUNT FORM\n" +
-                        "==============================\n");
+            System.out.println();
 
-                System.out.print("Enter Account number: ");
-                int accountNumber = sc.nextInt();
-                System.out.println("Your account number is "+accountNumber);
+            switch (opt){
 
-                System.out.println();
+                case 1:
+                    System.out.println(
+                            "==============================\n" +
+                            "NEW BANK ACCOUNT FORM\n" +
+                            "==============================\n");
 
-                System.out.print("Enter Customer name: ");
-                String customerName = sc.next();
-                sc.nextLine();
-                System.out.println("Your name is "+customerName);
+                    System.out.print("Enter Account number: ");
+                    int accountNumber = sc.nextInt();
+                    System.out.println("Your account number is "+accountNumber);
 
-                System.out.println();
+                    System.out.println();
 
-                System.out.print("Enter starting A/C balance: ");
-                double accountBalance = sc.nextDouble();
-                System.out.println("Your starting account balance is "+accountBalance);
+                    System.out.print("Enter Customer name: ");
+                    String customerName = sc.next();
+                    sc.nextLine();
+                    System.out.println("Your name is "+customerName);
 
-                System.out.println();
+                    System.out.println();
 
-                System.out.print("Enter Account password: ");
-                char[] password = sc.next().toCharArray();
-                System.out.print("Your password is ");
-                for (int i=0; i<password.length; i++){
-                    System.out.print(password[i]);
-                }
+                    System.out.print("Enter starting A/C balance: ");
+                    double accountBalance = sc.nextDouble();
+                    System.out.println("Your starting account balance is "+accountBalance);
 
-                System.out.println("\n\nBANK ACCOUNT NUMBER "+accountNumber+" SUCCESSFULLY CREATED");
+                    System.out.println();
 
-                BankAccount account = new BankAccount(
-                        accountNumber, accountBalance,
-                        customerName, password);
-                break;
+                    System.out.print("Enter Account password: ");
+                    char[] password = sc.next().toCharArray();
+                    System.out.print("Your password is ");
+                    for (int i=0; i<password.length; i++){
+                        System.out.print(password[i]);
+                    }
 
-            case 2:
-                break;
+                    System.out.println("\n\nBANK ACCOUNT NUMBER "+accountNumber+" SUCCESSFULLY CREATED");
+
+                    BankAccount account = new BankAccount(
+                            accountNumber, accountBalance,
+                            customerName, password);
+
+                    listOfBankAcc.add(account);
+
+                    break;
+
+                case 2:
+                    System.out.print("Enter your account number: ");
+                    int input_accNo = sc.nextInt();
+                    sc.nextLine();
+                    System.out.print("Enter your full name(in upper Camelcase): ");
+                    String input_customerName = sc.nextLine();
+
+                    System.out.println();
+
+                    for(BankAccount bankAccount: listOfBankAcc){
+                        if(bankAccount.getAccountNumber() == input_accNo
+                                && bankAccount.getCustomerName().equals(input_customerName)){
+                            System.out.println("Your account balance is: "+bankAccount.getAccountBalance());
+                        }
+                    }
+
+                    break;
+            }
         }
 
     }
