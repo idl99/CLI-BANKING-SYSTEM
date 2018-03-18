686847762ee62b82b660dcf8c95de1f675f21414
idl99 <ihandilnath2012@gmail.com>
2018-03-18 18:24:06 +0530

    revision of stage 5

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,25 +1,89 @@
-import java.io.Serializable;
+import java.io.IOException;
+import java.util.Scanner;
 
-public class BankAccount implements Serializable {
+public class BankAccount {
+    // Class defining Bank account object construct
 
-    public int accountNumber;
-    public double accountBalance;
-    public String customerName;
-    public char[] password;
-    public double interestRate;
-    public double autoDeposit;
-    public double autoWithdraw;
+    private static Scanner sc = new Scanner(System.in);
 
+    // Instance variables for Bank Account objects
+    private int accountNumber;
+    private double accountBalance;
 
-    public BankAccount(int accountNumber, double accountBalance, String customerName,
-                       char[] password, double interestRate, double autoDeposit, double autoWithdraw) {
+    // Static variables for Bank Account objects
+    public static double interestRate = 3;
+
+    private BankAccount(int accountNumber, double accountBalance) {
         this.accountNumber = accountNumber;
         this.accountBalance = accountBalance;
-        this.customerName = customerName;
-        this.password = password;
-        this.interestRate = interestRate;
-        this.autoDeposit = autoDeposit;
-        this.autoWithdraw = autoWithdraw;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public double getAccountBalance() {
+        return accountBalance;
+    }
+
+    public void setAccountBalance(double accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
+    public static double getInterestRate() {
+        return interestRate;
+    }
+
+    public static void setInterestRate(double interestRate) {
+        BankAccount.interestRate = interestRate;
+    }
+
+    public static BankAccount enterAccountData() throws IOException{
+
+        int accountNumber = 0;
+        System.out.print("Enter account number: ");
+        accountNumber = sc.nextInt();
+
+        if(accountNumber == 0)
+            return null;
+        else if(accountNumber<1000 || accountNumber > 9999)
+            throw new IOException("Invalid Account number. Value should be in " +
+                    "1000-9999");
+
+        double accountBalance = 0.0;
+        System.out.print("Enter account balance: ");
+        accountBalance = sc.nextDouble();
+
+        if(accountBalance<0 || accountBalance>100000)
+            throw new IOException("Invalid Account balance. Value should be in " +
+                    "$0.00-$100,000.00");
+
+        return new BankAccount(accountNumber,accountBalance);
+
+    }
+
+    public void computeInterest(int years){
+
+        System.out.printf("Account number: %d \n",this.accountNumber);
+
+        double balance = this.accountBalance;
+
+        for(int i=1; i<=years;i++){
+            double interest = balance * BankAccount.interestRate/100;
+            balance += interest;
+            System.out.printf("Balance at the end of year %d = %,.2f \n",i/12,balance);
+        }
+
+    }
+
+    public void displayAccount(){
+        System.out.printf("Account number: %d \n",this.accountNumber);
+
+        System.out.printf("Account balance: %,.2f \n",this.accountBalance);
     }
 
 }
---
+++ src/Old_BankAccount_5.java
@@ -0,0 +1,474 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Old_BankAccount_5 {
+
+    // Tester class for Bank Account objects
+
+    static Scanner sc = new Scanner(System.in);
+
+//    static List<BankAccount> listOfBankAcc; // Arraylist to store BankAccount objects
+
+    static OldBankAccount[] listOfBankAcc = new OldBankAccount[10];
+
+    static int noOfBankAcc = 0;
+
+    public static void main(String[] args){
+
+//        if(new File("Accounts.txt").exists()){
+//            listOfBankAcc = dataPersistency();
+//        } else {
+//            listOfBankAcc = new ArrayList<>();
+//        }
+
+        while (true) {
+
+            char opt;
+
+            System.out.print(
+                    "========================================\n"+
+                    "WELCOME TO INTERBANKING PTY SYSTEM\n"+
+                    "========================================\n"+
+                    "1. CREATE NEW BANK ACCOUNT \n"+
+                    "2. VIEW ACCOUNT BALANCE \n" +
+                    "3. ACCOUNT MONEY TRANSFER \n" +
+                    "4. DISPLAY ALL ACCOUNTS \n" +
+                    "5. SHOW BANK A/C BALANCE FORECAST \n" +
+                    "\nPLEASE ENTER MENU OPTION NUMBER OR 0 TO EXIT: "
+            );
+
+            opt = sc.nextLine().toCharArray()[0];
+
+            if(opt=='0') break;
+
+            System.out.println();
+
+            switch (opt){
+
+                case '1':
+
+                    {
+                        OldBankAccount account = null;
+
+                        try {
+
+                            for (int i = noOfBankAcc; i <10; i++) {
+
+                                account = enterAccountData();
+
+                                if (account == null) break;
+
+                                listOfBankAcc[i] = account;
+                                noOfBankAcc++;
+                                System.out.printf("\nBANK ACCOUNT NUMBER %d SUCCESSFULLY CREATED.\n\n", account.accountNumber);
+                                displayAccount(account);
+    //
+                                //  listOfBankAcc.add(account); // Adding newly created BankAccount to list of existing Bank Accounts
+    //                            dataPersistency(listOfBankAcc); // Writing to file
+                            }
+
+                            System.out.print("\nEnter number of years for which you wish to compute interest: ");
+                            int term = sc.nextInt();
+                            sc.nextLine();
+
+                            System.out.println();
+
+                            for (OldBankAccount forAccount : listOfBankAcc) {
+                                computeInterest(forAccount, term);
+                            }
+
+                        } catch (Exception e) {
+                            e.getMessage();
+                        }
+
+                    }
+
+                    break;
+
+                case '2':
+
+                    System.out.print("Enter account number: ");
+                    int input_accNo = sc.nextInt();
+                    sc.nextLine();
+                    System.out.print("Enter customer full name(in upper Camelcase): ");
+                    String input_customerName = sc.nextLine();
+
+                    System.out.println();
+
+                    boolean isFound = false; // true if BankAccount exists for input account number and name
+
+                    for(OldBankAccount bankAccount: listOfBankAcc){
+                        // Loop to iterate through list of existing Bank Accounts
+                        if(bankAccount.accountNumber == input_accNo
+                                && bankAccount.customerName.equals(input_customerName)){
+                            // Account exists
+                            System.out.print("Your account balance is: $");
+                            System.out.printf("%,.2f",bankAccount.accountBalance);
+                            System.out.println("\n");
+                            isFound = true;
+                        }
+                    }
+
+                    //existing BankAccount not found for input account number and name
+                    if(!isFound) System.out.println("Invalid account number or customer name. Please try again.\n");
+
+                    break;
+
+                case '3':
+                    // 1. Get account to transfer money from as fromAccount
+
+                    System.out.print("ENTER A/C NUMBER OF ACCOUNT TO TRANSFER MONEY FROM: ");
+                    int fromAccount = sc.nextInt();
+
+                    System.out.println();
+
+                    // 2. Get account to transfer money to as toAccount
+
+                    System.out.print("ENTER A/C NUMBER OF ACCOUNT TO TRANSFER MONEY TO: ");
+                    int toAccount = sc.nextInt();
+
+                    System.out.println();
+
+                    // 3. Get amount to transfer
+
+                    System.out.print("ENTER AMOUNT OF FUNDS TO TRANSFER: ");
+                    double transferVal = sc.nextDouble();
+                    sc.nextLine();
+
+                    System.out.println();
+
+                    try {
+                        processMoneyTransfer(fromAccount,toAccount,transferVal);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                        System.exit(0);
+                    } finally{
+                        System.out.println();
+                    }
+
+                    break;
+
+                case '4':
+
+                    for(OldBankAccount acc: listOfBankAcc){
+                        System.out.printf(
+                                "Account Number: %s \n" +
+                                "Account Balance: %,.2f \n" +
+                                "Customer Name: %s \n" +
+                                "Customer Password: %s \n\n",
+                                acc.accountNumber, acc.accountBalance, acc.customerName,String.valueOf(acc.password)
+                        );
+                    }
+
+                    break;
+
+                case '5':
+
+                    OldBankAccount account = null;
+
+                    try {
+
+                        System.out.print("Enter bank account number: ");
+                        int accountNumber;
+                        accountNumber = sc.nextInt();
+                        sc.nextLine();
+
+                        boolean accIsExist = false;
+                        for(OldBankAccount searchAcc: listOfBankAcc){
+                            if (searchAcc.accountNumber == accountNumber) {
+                                account = searchAcc;
+                                accIsExist = true;
+                            }
+                        }
+
+                        if(!accIsExist) {
+                            throw new Exception("THE ACCOUNT NUMBER YOU ENTERED DOESN'T EXIST");
+                        } else {
+                            System.out.print("Enter the number of months for forecast: ");
+                            int term = sc.nextInt();
+                            sc.nextLine();
+                            System.out.println();
+                            showAccBalForecast(account, term);
+                        }
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                    break;
+
+                default:
+                    System.out.println("Invalid Input. Please re-enter menu option below.");
+            }
+        }
+
+    }
+
+    public static void processMoneyTransfer (int paramFrom, int paramToAcc, double transactVal)
+            throws Exception{
+
+        if(paramFrom == paramToAcc){
+            throw new Exception("Both account numbers are same");
+        }
+
+        OldBankAccount fromBankAcc = null;
+        OldBankAccount toBankAcc = null;
+
+        for(OldBankAccount acc : listOfBankAcc){
+            if(acc.accountNumber == paramFrom) fromBankAcc = acc;
+            else if(acc.accountNumber == paramToAcc) toBankAcc = acc;
+        }
+
+        if(fromBankAcc == null) {
+            throw new Exception("THE ACCOUNT NUMBER FROM WHICH YOU ARE " +
+                    "TRANSFERRING FUNDS DOESN'T EXIST");
+        } else if(toBankAcc == null) {
+            throw new Exception("THE ACCOUNT NUMBER TO YOU ARE " +
+                    "TRANSFERRING FUNDS DOESN'T EXIST");
+        }
+
+        // 4.    If fromAccount balance falls below $0 after deduction
+        //          then cancel transaction
+        if(fromBankAcc.accountBalance - transactVal < 0) throw new Exception("Insufficient funds in account. " +
+                "Account Balance is "+String.valueOf(fromBankAcc.accountBalance));
+
+        // 5.    If fromAccount balance falls below $10
+        //          then issue warning message about low a/c balance
+        if(fromBankAcc.accountBalance - transactVal <= 10) System.out.println("WARNING: The balance of the account " +
+                "from which you are transferring funds to has dropped below $10.\n");
+
+        // 6.    If toAccount balance falls above $100,000
+        //          then issue warning message about federal insurance
+        if (toBankAcc.accountBalance + transactVal > 100000) System.out.println("WARNING: The balance of the account " +
+                "to which you are transferring funds to has gone beyond the maximum federally insured value.\n");
+
+        // 7.    Display the ending messages
+        //
+        // NOTE: For errors, program should terminate GRACEFULLY, for warning
+        //          messages, the program continues to execute
+
+        fromBankAcc.accountBalance -= transactVal;
+        toBankAcc.accountBalance += transactVal;
+
+        System.out.printf("AMOUNT OF %,.2f SUCCESSFULLY TRANSFERRED FROM ACCOUNT NUMBER %d to %d.\n\n" +
+                        "TRANSFERRER'S NEW ACCOUNT BALANCE IS %,.2f\n\n" +
+                        "RECIPIENT'S NEW ACCOUNT BALANCE IS %,.2f",
+                transactVal,paramFrom,paramToAcc,fromBankAcc.accountBalance, toBankAcc.accountBalance);
+
+    }
+
+    public static void showAccBalForecast(OldBankAccount account, int months){
+
+        System.out.println("THE BANK ACCOUNT BALANCE FORECAST IS AS FOLLOWS: \n");
+
+        int year = 0;
+
+        double balance = account.accountBalance;
+
+        for(int i=0; i<months; i++){
+
+            if(i%12==0){
+                year++;
+                System.out.printf("" +
+                        "=========================\n" +
+                        "YEAR NUMBER %d \n" +
+                        "=========================\n" +
+                        "\n",year);
+            }
+
+            System.out.printf("----- Year %d Month %d-----\n",year,(i%12)+1);
+            System.out.printf("Month Starting Balance: %,.2f \n",balance);
+
+            double principal = balance+account.autoDeposit-account.autoWithdraw;
+
+            double interest = principal * ((account.interestRate/12)/100) ;
+
+            balance = principal + interest;
+
+            System.out.printf("Month Ending Balance: %,.2f \n\n",balance);
+
+        }
+
+        System.out.println();
+
+    }
+
+    public static ArrayList<OldBankAccount> dataPersistency(){
+        // METHOD FOR READING FROM FILE
+        FileInputStream fis = null;
+        ObjectInputStream ois = null;
+        ArrayList<OldBankAccount> listOfBankAcc = null;
+
+        try {
+            fis = new FileInputStream(new File("Accounts.txt"));
+            ois = new ObjectInputStream(fis);
+            listOfBankAcc = (ArrayList<OldBankAccount>) ois.readObject();
+        } catch (FileNotFoundException e) {
+            System.out.println("No Bank Accounts created yet");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                ois.close();
+                fis.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (NullPointerException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return listOfBankAcc;
+
+    }
+
+    public static void dataPersistency(List<OldBankAccount> listOfAccounts){
+        // METHOD FOR WRITING TO FILE
+        FileOutputStream fos = null;
+
+        ObjectOutputStream oos = null;
+
+        try {
+            fos = new FileOutputStream(new File("Accounts.txt"));
+            oos = new ObjectOutputStream(fos);
+            oos.writeObject(listOfAccounts);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                oos.close();
+                fos.close();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public static OldBankAccount enterAccountData() throws Exception{
+        System.out.println(
+                "==============================\n" +
+                        "NEW BANK ACCOUNT FORM\n" +
+                        "==============================\n");
+
+        // =============================================
+        // Getting input for account number
+        // =============================================
+
+        System.out.print("Enter Account number: ");
+        int accountNumber = sc.nextInt();
+        sc.nextLine();
+        if(accountNumber == 0){
+            return null;
+        } else if(!(accountNumber>1000 && accountNumber<9999)){
+            throw new Exception("\nInvalid account number. Account number should be between" +
+                    " 1000 and 10000\n");
+        }
+
+        // =============================================
+        // Getting input for starting account balance
+        // =============================================
+
+        System.out.print("Enter starting A/C balance: ");
+        double accountBalance = sc.nextDouble();
+        sc.nextLine();
+        if(accountBalance<=0) {
+            throw new Exception("\nInvalid Starting A/C balance. Balance should be positive\n");
+        } else if(accountBalance>100000){
+            throw new Exception("\nAccount balance cannot be greater than $100,000 USD");
+        }
+
+        // =============================================
+        // Getting input for customer name
+        // =============================================
+
+        System.out.print("Enter Customer name: ");
+        String customerName = sc.nextLine();
+
+        // =============================================
+        // Getting input for account password
+        // =============================================
+
+        System.out.print("Enter Account password: ");
+        char[] password = sc.next().toCharArray();
+
+        // =============================================
+        // Getting input for account password
+        // =============================================
+
+        System.out.print("Enter Account interest rate: ");
+        double interestRate = sc.nextDouble();
+
+        if(interestRate < 0.01 || interestRate > 15) {
+            sc.nextLine();
+            throw new Exception("Invalid Account interest rate. Rate should be between 0.01%" +
+                    " and 15%");
+        }
+
+        // =============================================
+        // Getting input for aut deposit amount
+        // =============================================
+
+        System.out.print("Enter monthly auto deposit amount: ");
+        double autoDeposit = sc.nextDouble();
+
+        // =============================================
+        // Getting input for auto withdraw amount
+        // =============================================
+
+        System.out.print("Enter monthly withdraw amount: ");
+        double autoWithdraw = sc.nextDouble();
+        sc.nextLine();
+
+        return new OldBankAccount(accountNumber, accountBalance, customerName,
+                password, interestRate, autoDeposit, autoWithdraw);
+    }
+
+    public static void computeInterest(OldBankAccount account, int noOfYears){
+
+        System.out.printf("COMPUTE ACCOUNT INTEREST FOR %d\n\n",account.accountNumber);
+
+        double balance = account.accountBalance;
+
+        int months = noOfYears * 12;
+
+        for(int i=1; i<=months; i++){
+
+            double principal = balance+account.autoDeposit-account.autoWithdraw;
+
+            double interest = principal * ((account.interestRate/12)/100) ;
+
+            balance = principal + interest;
+
+            if(i%12==0) System.out.printf("Account balance at the end of year %d is %,.2f\n",(i/12),balance);
+
+        }
+
+        System.out.printf("Account balance at the end of the term is %,.2f\n",balance);
+
+        System.out.println();
+
+    }
+
+    public static void displayAccount(OldBankAccount account){
+
+        System.out.printf("ACCOUNT NUMBER: %d \n",account.accountNumber);
+
+        System.out.printf("ACCOUNT BALANCE: %,.2f \n",account.accountBalance);
+
+        System.out.printf("CUSTOMER NAME: %s \n",account.customerName);
+
+        System.out.printf("INTEREST RATE: %.2f %% \n",account.interestRate);
+
+        System.out.printf("MONTHLY AUTO DEPOSIT AMOUNT: %,.2f \n",account.autoDeposit);
+
+        System.out.printf("MONTHLY AUTO WITHDRAW AMOUNT: %,.2f \n",account.autoWithdraw);
+
+    }
+
+
+
+}
