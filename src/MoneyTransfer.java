785f43d385061dc582b13f2bdbeca3de92f4254d
idl99 <ihandilnath2012@gmail.com>
2018-03-24 21:32:15 +0530

    reformatted code, removed unnecessary javadoc methods, unnecessary rollback method

--- src/MoneyTransfer.java
+++ src/MoneyTransfer.java
@@ -1,49 +1,28 @@
-/**
- * @author Ihan Lelwala
- */
 public class MoneyTransfer {
 
+    // Instance variables
     private BankAccount transferrer;
-
     private BankAccount recipient;
-
     private double amount;
 
-    /**
-     * @param transferrer
-     * @param recipient
-     * @param amount
-     */
     public MoneyTransfer(BankAccount transferrer, BankAccount recipient, double amount) {
         this.transferrer = transferrer;
         this.recipient = recipient;
         this.amount = amount;
     }
 
-    /**
-     * @return
-     */
     public BankAccount getTransferrer() {
         return this.transferrer;
     }
 
-    /**
-     * @return
-     */
     public BankAccount getRecipient() {
         return this.recipient;
     }
 
-    /**
-     * @return
-     */
     public double getAmount() {
         return this.amount;
     }
 
-    /**
-     * @return
-     */
     public void process() throws IllegalBankAccountOperation{
 
         if(transferrer.equals(recipient)){
@@ -53,8 +32,8 @@ public class MoneyTransfer {
         if(transferrer.getAccountBalance() - this.amount < 0) throw new IllegalBankAccountOperation("Insufficient funds in account. " +
                 "Account Balance is "+String.valueOf(transferrer.getAccountBalance()));
 
-
         transferrer.decrementBalance(amount);
+
         recipient.incrementBalance(amount);
 
         System.out.printf("AMOUNT OF %,.2f SUCCESSFULLY TRANSFERRED FROM ACCOUNT NUMBER %d to %d.\n\n" +
@@ -62,13 +41,7 @@ public class MoneyTransfer {
                         "RECIPIENT'S NEW ACCOUNT BALANCE IS %,.2f\n",
                 amount,transferrer.getAccountNumber(),recipient.getAccountNumber(),
                 transferrer.getAccountBalance(), recipient.getAccountBalance());
-    }
 
-    /**
-     * @return
-     */
-    public void rollback() {
-        // TODO implement here
     }
 
 }
